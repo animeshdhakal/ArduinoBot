f71151f4dc1b58b2a59021a51e6d930ca7fc0d80
Animesh Dhakal <dhakalanimesh385@gmail.com>
2023-02-01 21:05:47 +0545

    Fixed ForwardLeft and Right

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,6 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
-SoftwareSerial BTSerial(2, 3); // RX, TX
-
 #define MOTOR1_SPEED 5
 #define MOTOR1_1 6
 #define MOTOR1_2 7
@@ -11,7 +9,17 @@ SoftwareSerial BTSerial(2, 3); // RX, TX
 #define MOTOR2_1 9
 #define MOTOR2_2 8
 
-void setup() {
+#define BLUETOOTH_RX 2
+#define BLUETOOTH_TX 3
+
+#define SPEED_COEF 3
+
+SoftwareSerial BTSerial(BLUETOOTH_RX, BLUETOOTH_TX);
+
+int speed = 255;
+
+void setup()
+{
   pinMode(LED_BUILTIN, OUTPUT);
 
   BTSerial.begin(9600);
@@ -24,153 +32,174 @@ void setup() {
   pinMode(MOTOR2_SPEED, OUTPUT);
   pinMode(MOTOR2_1, OUTPUT);
   pinMode(MOTOR2_2, OUTPUT);
-
-  analogWrite(MOTOR1_SPEED, 100);
-  digitalWrite(MOTOR1_1, LOW);
-  digitalWrite(MOTOR1_2, HIGH);
-
-  analogWrite(MOTOR2_SPEED, 100);
-  digitalWrite(MOTOR2_1, LOW);
-  digitalWrite(MOTOR2_2, HIGH);
-
 }
 
-void changeSpeed(int pin, int speed){
-  analogWrite(pin, speed);
-}
-
-void stop(){
+void stop()
+{
   digitalWrite(MOTOR1_1, LOW);
   digitalWrite(MOTOR1_2, LOW);
+  analogWrite(MOTOR1_SPEED, 0);
+
   digitalWrite(MOTOR2_1, LOW);
   digitalWrite(MOTOR2_2, LOW);
+  analogWrite(MOTOR2_SPEED, 0);
 }
 
-void forward(){
+void forward()
+{
   digitalWrite(MOTOR1_1, LOW);
   digitalWrite(MOTOR1_2, HIGH);
+  analogWrite(MOTOR1_SPEED, speed);
+
   digitalWrite(MOTOR2_1, LOW);
   digitalWrite(MOTOR2_2, HIGH);
+  analogWrite(MOTOR2_SPEED, speed);
 }
 
-void backward(){
+void backward()
+{
   digitalWrite(MOTOR1_1, HIGH);
   digitalWrite(MOTOR1_2, LOW);
+  analogWrite(MOTOR1_SPEED, speed);
+
   digitalWrite(MOTOR2_1, HIGH);
   digitalWrite(MOTOR2_2, LOW);
+  analogWrite(MOTOR2_SPEED, speed);
 }
 
-void left(){
+void left()
+{
   digitalWrite(MOTOR1_1, HIGH);
   digitalWrite(MOTOR1_2, LOW);
+  analogWrite(MOTOR1_SPEED, speed);
+
   digitalWrite(MOTOR2_1, LOW);
   digitalWrite(MOTOR2_2, HIGH);
+  analogWrite(MOTOR2_SPEED, speed);
 }
 
-void right(){
+void right()
+{
   digitalWrite(MOTOR1_1, LOW);
   digitalWrite(MOTOR1_2, HIGH);
+  analogWrite(MOTOR1_SPEED, speed);
+
   digitalWrite(MOTOR2_1, HIGH);
   digitalWrite(MOTOR2_2, LOW);
+  analogWrite(MOTOR2_SPEED, speed);
 }
 
-void forwardLeft(){
-  digitalWrite(MOTOR1_1, LOW);
-  digitalWrite(MOTOR1_2, LOW);
-  digitalWrite(MOTOR2_1, LOW);
-  digitalWrite(MOTOR2_2, HIGH);
-}
-
-void forwardRight(){
+void forwardLeft()
+{
   digitalWrite(MOTOR1_1, LOW);
   digitalWrite(MOTOR1_2, HIGH);
+  analogWrite(MOTOR1_SPEED, speed);
+
   digitalWrite(MOTOR2_1, LOW);
-  digitalWrite(MOTOR2_2, LOW);
+  digitalWrite(MOTOR2_2, HIGH);
+  analogWrite(MOTOR2_SPEED, speed / SPEED_COEF);
 }
 
-void backwardLeft(){
+void forwardRight()
+{
+  digitalWrite(MOTOR1_1, LOW);
+  digitalWrite(MOTOR1_2, HIGH);
+  analogWrite(MOTOR1_SPEED, speed / SPEED_COEF);
+
+  digitalWrite(MOTOR2_1, LOW);
+  digitalWrite(MOTOR2_2, HIGH);
+  analogWrite(MOTOR2_SPEED, speed);
+}
+
+void backwardLeft()
+{
   digitalWrite(MOTOR1_1, HIGH);
   digitalWrite(MOTOR1_2, LOW);
-  digitalWrite(MOTOR2_1, LOW);
-  digitalWrite(MOTOR2_2, LOW);
-}
+  analogWrite(MOTOR1_SPEED, speed);
 
-void backwardRight(){
-  digitalWrite(MOTOR1_1, LOW);
-  digitalWrite(MOTOR1_2, LOW);
   digitalWrite(MOTOR2_1, HIGH);
   digitalWrite(MOTOR2_2, LOW);
+  analogWrite(MOTOR2_SPEED, speed / SPEED_COEF);
 }
 
-void changeSpeed(int speed){
-  changeSpeed(MOTOR1_SPEED, speed);
-  changeSpeed(MOTOR2_SPEED, speed);
+void backwardRight()
+{
+  digitalWrite(MOTOR1_1, HIGH);
+  digitalWrite(MOTOR1_2, LOW);
+  analogWrite(MOTOR1_SPEED, speed / SPEED_COEF);
+
+  digitalWrite(MOTOR2_1, HIGH);
+  digitalWrite(MOTOR2_2, LOW);
+  analogWrite(MOTOR2_SPEED, speed);
 }
 
-void loop() {
-  if (BTSerial.available()) {
-      char command = BTSerial.read();
+void loop()
+{
+  if (BTSerial.available())
+  {
+    char command = BTSerial.read();
 
-      stop();
+    stop();
 
-      switch (command) {
-        case 'F':
-          forward();
-          break;
-        case 'B':
-          backward();
-          break;
-        case 'L':
-          left();
-          break;
-        case 'R':
-          right();
-          break;
-        case 'G':
-          forwardLeft();
-          break;
-        case 'I':
-          forwardRight();
-          break;
-        case 'H':
-          backwardLeft();
-          break;
-        case 'J':
-          backwardRight();
-          break;
-        case '0':
-          changeSpeed(100);
-          break;
-        case '1':
-          changeSpeed(140);
-          break;
-        case '2':
-          changeSpeed(153);
-          break;
-        case '3':
-          changeSpeed(165);
-          break;
-        case '4':
-          changeSpeed(178);
-          break;
-        case '5':
-          changeSpeed(191);
-          break;
-        case '6':
-          changeSpeed(204);
-          break;
-        case '7':
-          changeSpeed(216);
-          break;
-        case '8':
-          changeSpeed(229);
-          break;
-        case '9':
-          changeSpeed(242);
-          break;
-        case 'q':
-          changeSpeed(255);
-          break;
-      }   
+    switch (command)
+    {
+    case 'F':
+      forward();
+      break;
+    case 'B':
+      backward();
+      break;
+    case 'L':
+      left();
+      break;
+    case 'R':
+      right();
+      break;
+    case 'G':
+      forwardLeft();
+      break;
+    case 'I':
+      forwardRight();
+      break;
+    case 'H':
+      backwardLeft();
+      break;
+    case 'J':
+      backwardRight();
+      break;
+    case '0':
+      speed = 100;
+      break;
+    case '1':
+      speed = 140;
+      break;
+    case '2':
+      speed = 153;
+      break;
+    case '3':
+      speed = 165;
+      break;
+    case '4':
+      speed = 178;
+      break;
+    case '5':
+      speed = 191;
+      break;
+    case '6':
+      speed = 204;
+      break;
+    case '7':
+      speed = 216;
+      break;
+    case '8':
+      speed = 229;
+      break;
+    case '9':
+      speed = 242;
+      break;
+    case 'q':
+      speed = 255;
+      break;
+    }
   }
 }
